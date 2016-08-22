18f97fa43a9ad0ec2ff8eb179c80f2d6e8f491e8
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-08-22 21:53:26 +0100

    Vendor current dependencies

---
+++ vendor/github.com/heketi/heketi/apps/glusterfs/brick_entry_test.go
@@ -0,0 +1,225 @@
+//
+// Copyright (c) 2015 The heketi Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package glusterfs
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/boltdb/bolt"
+	"github.com/heketi/heketi/executors"
+	"github.com/heketi/heketi/pkg/glusterfs/api"
+	"github.com/heketi/tests"
+)
+
+func TestNewBrickEntry(t *testing.T) {
+
+	size := uint64(10)
+	tpsize := size * 2
+	deviceid := "abc"
+	nodeid := "def"
+	ps := size
+
+	b := NewBrickEntry(size, tpsize, ps, deviceid, nodeid)
+	tests.Assert(t, b.Info.Id != "")
+	tests.Assert(t, b.TpSize == tpsize)
+	tests.Assert(t, b.PoolMetadataSize == ps)
+	tests.Assert(t, b.Info.DeviceId == deviceid)
+	tests.Assert(t, b.Info.NodeId == nodeid)
+	tests.Assert(t, b.Info.Size == size)
+}
+
+func TestBrickEntryMarshal(t *testing.T) {
+	size := uint64(10)
+	tpsize := size * 2
+	deviceid := "abc"
+	nodeid := "def"
+	ps := size
+	m := NewBrickEntry(size, tpsize, ps, deviceid, nodeid)
+
+	buffer, err := m.Marshal()
+	tests.Assert(t, err == nil)
+	tests.Assert(t, buffer != nil)
+	tests.Assert(t, len(buffer) > 0)
+
+	um := &BrickEntry{}
+	err = um.Unmarshal(buffer)
+	tests.Assert(t, err == nil)
+
+	tests.Assert(t, reflect.DeepEqual(um, m))
+}
+
+func TestNewBrickEntryFromIdNotFound(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	// Test for ID not found
+	err := app.db.View(func(tx *bolt.Tx) error {
+		_, err := NewBrickEntryFromId(tx, "123")
+		return err
+	})
+	tests.Assert(t, err == ErrNotFound)
+
+}
+
+func TestNewBrickEntryFromId(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	// Create a brick
+	b := NewBrickEntry(10, 20, 5, "abc", "def")
+
+	// Save element in database
+	err := app.db.Update(func(tx *bolt.Tx) error {
+		return b.Save(tx)
+	})
+	tests.Assert(t, err == nil)
+
+	var brick *BrickEntry
+	err = app.db.View(func(tx *bolt.Tx) error {
+		var err error
+		brick, err = NewBrickEntryFromId(tx, b.Info.Id)
+		return err
+	})
+	tests.Assert(t, err == nil)
+	tests.Assert(t, reflect.DeepEqual(brick, b))
+
+}
+
+func TestNewBrickEntrySaveDelete(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	// Create a brick
+	b := NewBrickEntry(10, 20, 5, "abc", "def")
+
+	// Save element in database
+	err := app.db.Update(func(tx *bolt.Tx) error {
+		return b.Save(tx)
+	})
+	tests.Assert(t, err == nil)
+
+	// Delete entry which has devices
+	var brick *BrickEntry
+	err = app.db.Update(func(tx *bolt.Tx) error {
+		var err error
+		brick, err = NewBrickEntryFromId(tx, b.Info.Id)
+		if err != nil {
+			return err
+		}
+
+		err = brick.Delete(tx)
+		if err != nil {
+			return err
+		}
+
+		return nil
+
+	})
+	tests.Assert(t, err == nil)
+
+	// Check brick has been deleted and is not in db
+	err = app.db.View(func(tx *bolt.Tx) error {
+		var err error
+		brick, err = NewBrickEntryFromId(tx, b.Info.Id)
+		return err
+	})
+	tests.Assert(t, err == ErrNotFound)
+}
+
+func TestNewBrickEntryNewInfoResponse(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	// Create a brick
+	b := NewBrickEntry(10, 20, 5, "abc", "def")
+
+	// Save element in database
+	err := app.db.Update(func(tx *bolt.Tx) error {
+		return b.Save(tx)
+	})
+	tests.Assert(t, err == nil)
+
+	var info *api.BrickInfo
+	err = app.db.View(func(tx *bolt.Tx) error {
+		brick, err := NewBrickEntryFromId(tx, b.Id())
+		if err != nil {
+			return err
+		}
+
+		info, err = brick.NewInfoResponse(tx)
+		return err
+	})
+	tests.Assert(t, err == nil)
+	tests.Assert(t, reflect.DeepEqual(*info, b.Info))
+}
+
+func TestBrickEntryDestroyCheck(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	// Create the app
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	// Create a brick
+	b := NewBrickEntry(10, 20, 5, "abc", "node")
+	n := NewNodeEntry()
+	n.Info.Id = "node"
+	n.Info.Hostnames.Manage = []string{"manage"}
+	n.Info.Hostnames.Storage = []string{"storage"}
+
+	// Save element in database
+	err := app.db.Update(func(tx *bolt.Tx) error {
+		err := n.Save(tx)
+		tests.Assert(t, err == nil)
+		return b.Save(tx)
+	})
+	tests.Assert(t, err == nil)
+
+	app.xo.MockBrickDestroyCheck = func(host string, brick *executors.BrickRequest) error {
+		return fmt.Errorf("MOCK error")
+	}
+
+	err = b.DestroyCheck(app.db, app.executor)
+	tests.Assert(t, err != nil)
+
+	app.xo.MockBrickDestroyCheck = func(host string, brick *executors.BrickRequest) error {
+		return nil
+	}
+
+	err = b.DestroyCheck(app.db, app.executor)
+	tests.Assert(t, err == nil, err)
+}
